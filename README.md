6420dd3ed24b288a1acf475a8510b2566227ef30
andrea-couto <acouto@student.bridgew.edu>
2018-05-14 19:03:17 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+"# Photo-Flora-Size"
+
+This project is intended to show plant growth over a period of time using openCV.<br><br>
+
+We have a program that will take a photo and reference width as arguments.
+returnPlantSize.py uses openCV to recognize the plant in the photo and use a reference
+object with a known width in inches to measure the plant.<br><br>
+
+To run returnPlantSize.py in the command line with a photo called plantImage and a quarter as a 
+reference object:<br>
+
+python returnPlantSize.py --image plantImage.jpg --width 0.955<br><br>
+
+
+
+
---
+++ returnPlantSize.py
@@ -0,0 +1,142 @@
+import cv2
+import numpy as np
+import argparse
+from scipy.spatial import distance as dist
+from imutils import contours
+import imutils
+from imutils import perspective
+
+
+def midpoint(ptA, ptB):
+    return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+                help="path to the input image")
+ap.add_argument("-w", "--width", type=float, required=True,
+                help="width of the left-most object in the image (in inches)")
+args = vars(ap.parse_args())
+
+
+lowerBound = np.array([33, 80, 40])
+upperBound = np.array([102, 255, 255])
+
+img = cv2.imread(args["image"],1)
+
+kernelOpen = np.ones((1, 1))
+kernelClose = np.ones((20, 20))
+
+img = cv2.resize(img, (340, 220))
+
+# FOR GETTING GREEN CONTOUR
+
+# convert BGR to HSV
+imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+# create the Mask
+mask = cv2.inRange(imgHSV, lowerBound, upperBound)
+# morphology
+maskOpen = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernelOpen)
+maskClose = cv2.morphologyEx(maskOpen, cv2.MORPH_CLOSE, kernelClose)
+
+maskFinal = maskClose
+_, conts, h = cv2.findContours(maskFinal.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+# END GETTING GREEN CONTOUR
+
+gray = cv2.GaussianBlur(img, (7, 7), 0)
+
+# perform edge detection, then perform a dilation + erosion to
+# close gaps in between object edges
+edged = cv2.Canny(gray, 50, 100)
+edged = cv2.dilate(edged, None, iterations=1)
+edged = cv2.erode(edged, None, iterations=1)
+
+# find contours in the edge map
+Othercnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+ref = Othercnts[0] if imutils.is_cv2() else Othercnts[1]
+
+ref = ref[0]
+# END GETTING REFERENCES
+
+# here also get the contour of the reference object conts.add(reference obj)
+
+(cnts, _) = contours.sort_contours(conts)
+pixelsPerMetric = None
+
+cnts = (ref,)+cnts
+print(len(cnts))
+
+
+for c in cnts:
+    # if the contour is not sufficiently large, ignore it
+    if cv2.contourArea(c) < 100:
+        continue
+
+    # compute the rotated bounding box of the contour
+    orig = img.copy()
+    box = cv2.minAreaRect(c)
+    box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
+    box = np.array(box, dtype="int")
+
+    # order the points in the contour such that they appear
+    # in top-left, top-right, bottom-right, and bottom-left
+    # order, then draw the outline of the rotated bounding
+    # box
+    box = perspective.order_points(box)
+    cv2.drawContours(orig, [box.astype("int")], -1, (0, 255, 0), 2)
+
+    # loop over the original points and draw them
+    for (x, y) in box:
+        cv2.circle(orig, (int(x), int(y)), 5, (0, 0, 255), -1)
+        # unpack the ordered bounding box, then compute the midpoint
+        # between the top-left and top-right coordinates, followed by
+        # the midpoint between bottom-left and bottom-right coordinates
+        (tl, tr, br, bl) = box
+        (tltrX, tltrY) = midpoint(tl, tr)
+        (blbrX, blbrY) = midpoint(bl, br)
+
+        # compute the midpoint between the top-left and top-right points,
+        # followed by the midpoint between the top-righ and bottom-right
+        (tlblX, tlblY) = midpoint(tl, bl)
+        (trbrX, trbrY) = midpoint(tr, br)
+
+        # draw the midpoints on the image
+        cv2.circle(orig, (int(tltrX), int(tltrY)), 5, (255, 0, 0), -1)
+        cv2.circle(orig, (int(blbrX), int(blbrY)), 5, (255, 0, 0), -1)
+        cv2.circle(orig, (int(tlblX), int(tlblY)), 5, (255, 0, 0), -1)
+        cv2.circle(orig, (int(trbrX), int(trbrY)), 5, (255, 0, 0), -1)
+
+        # draw lines between the midpoints
+        cv2.line(orig, (int(tltrX), int(tltrY)), (int(blbrX), int(blbrY)),
+                 (255, 0, 255), 2)
+        cv2.line(orig, (int(tlblX), int(tlblY)), (int(trbrX), int(trbrY)),
+                 (255, 0, 255), 2)
+
+        # compute the Euclidean distance between the midpoints
+        dA = dist.euclidean((tltrX, tltrY), (blbrX, blbrY))
+        dB = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
+
+        # if the pixels per metric has not been initialized, then
+        # compute it as the ratio of pixels to supplied metric
+        # (in this case, inches)
+        if pixelsPerMetric is None:
+            pixelsPerMetric = dB / args["width"]
+
+            # compute the size of the object
+        dimA = dA / pixelsPerMetric
+        dimB = dB / pixelsPerMetric
+
+        # draw the object sizes on the image
+        cv2.putText(orig, "{:.1f}in".format(dimA),
+                    (int(tltrX - 15), int(tltrY - 10)), cv2.FONT_HERSHEY_SIMPLEX,
+                    0.65, (0, 0, 0), 2)
+
+        cv2.putText(orig, "{:.1f}in".format(dimB),
+                    (int(trbrX + 10), int(trbrY)), cv2.FONT_HERSHEY_SIMPLEX,
+                    0.65, (0, 0, 0), 2)
+
+        #python test1.py --image testPlant.jpg --width 0.955
+
+    cv2.imshow("final", orig)
+    cv2.waitKey(0)
