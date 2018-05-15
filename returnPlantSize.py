a4802ec82175c9c058338516efae921879728370
andrea-couto <acouto@student.bridgew.edu>
2018-05-14 20:43:52 -0400

    swapping height and width values

--- returnPlantSize.py
+++ returnPlantSize.py
@@ -128,11 +128,11 @@ for c in cnts:
         dimB = dB / pixelsPerMetric
 
         # draw the object sizes on the image
-        cv2.putText(orig, "{:.1f}in".format(dimA),
+        cv2.putText(orig, "{:.1f}in".format(dimB),
                     (int(tltrX - 15), int(tltrY - 10)), cv2.FONT_HERSHEY_SIMPLEX,
                     0.65, (0, 0, 0), 2)
 
-        cv2.putText(orig, "{:.1f}in".format(dimB),
+        cv2.putText(orig, "{:.1f}in".format(dimA),
                     (int(trbrX + 10), int(trbrY)), cv2.FONT_HERSHEY_SIMPLEX,
                     0.65, (0, 0, 0), 2)
 
