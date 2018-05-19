7789672ced0d04536a724dc5ec5ce4554e5b54ec
andrea-couto <acouto@student.bridgew.edu>
2018-05-19 18:48:38 -0400

    more cleaning returnPlantSize

--- returnPlantSize.py
+++ returnPlantSize.py
@@ -23,6 +23,7 @@ kernelClose = np.ones((20, 20))
 def midpoint(ptA, ptB):
     return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
 
+
 def makeCommandArguments():
     ap = argparse.ArgumentParser()
     ap.add_argument("-i", "--image", required=True,
@@ -31,6 +32,7 @@ def makeCommandArguments():
                     help="width of the left-most object in the image (in inches)")
     args = vars(ap.parse_args())
     return args
+
 
 def returnGreenObjects(img):
     imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
@@ -42,6 +44,7 @@ def returnGreenObjects(img):
     _, conts, h = cv2.findContours(maskFinal.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     return conts
 
+
 def returnReferenceObject(img):
     gray = cv2.GaussianBlur(img, (7, 7), 0)
     edged = cv2.Canny(gray, 50, 100)
@@ -52,33 +55,47 @@ def returnReferenceObject(img):
     ref = ref[0]
     return ref
 
+
 def computeBoundingBox(contobj):
     box = cv2.minAreaRect(contobj)
     box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
     box = np.array(box, dtype="int")
     return box
 
+
 def computeTopBottomMidpoints(tl, tr, bl, br):
     (tltrX, tltrY) = midpoint(tl, tr)
     (blbrX, blbrY) = midpoint(bl, br)
     return (tltrX, tltrY), (blbrX, blbrY)
+
 
 def computeSideMidpoints(tl, tr, bl, br):
     (tlblX, tlblY) = midpoint(tl, bl)
     (trbrX, trbrY) = midpoint(tr, br)
     return (tlblX, tlblY), (trbrX, trbrY)
 
-def drawMidpoints(orig, tltrX, tltrY, tlblX, tlblY,blbrX,blbrY,trbrX,trbrY):
+
+def drawMidpoints(orig,tltrX,tltrY,tlblX,tlblY,blbrX,blbrY,trbrX,trbrY):
     cv2.circle(orig, (int(tltrX), int(tltrY)), 5, (255, 0, 0), -1)
     cv2.circle(orig, (int(blbrX), int(blbrY)), 5, (255, 0, 0), -1)
     cv2.circle(orig, (int(tlblX), int(tlblY)), 5, (255, 0, 0), -1)
     cv2.circle(orig, (int(trbrX), int(trbrY)), 5, (255, 0, 0), -1)
 
-def drawMidpointLines(orig, tltrX, tltrY, blbrX, blbrY,tlblX,tlblY,trbrX,trbrY):
+
+def drawMidpointLines(orig,tltrX,tltrY,blbrX,blbrY,tlblX,tlblY,trbrX,trbrY):
     cv2.line(orig, (int(tltrX), int(tltrY)), (int(blbrX), int(blbrY)),
              (255, 0, 255), 2)
     cv2.line(orig, (int(tlblX), int(tlblY)), (int(trbrX), int(trbrY)),
              (255, 0, 255), 2)
+
+
+def drawSizes(orig, dimB, dimA,tltrX,tltrY,trbrX,trbrY):
+    cv2.putText(orig, "{:.1f}in".format(dimB),
+            (int(tltrX - 15), int(tltrY - 10)), cv2.FONT_HERSHEY_SIMPLEX,
+            0.65, (0, 0, 0), 2)
+    cv2.putText(orig, "{:.1f}in".format(dimA),
+                (int(trbrX + 10), int(trbrY)), cv2.FONT_HERSHEY_SIMPLEX,
+                0.65, (0, 0, 0), 2)
 
 def main():
     args = makeCommandArguments()
@@ -117,21 +134,12 @@ def main():
             if pixelsPerMetric is None:
                 pixelsPerMetric = dB / args["width"] #in inches
 
-            # compute the size of the object
             dimA = dA / pixelsPerMetric
             dimB = dB / pixelsPerMetric
 
-            # draw the object sizes on the image
-            cv2.putText(orig, "{:.1f}in".format(dimB),
-                        (int(tltrX - 15), int(tltrY - 10)), cv2.FONT_HERSHEY_SIMPLEX,
-                        0.65, (0, 0, 0), 2)
+            drawSizes(orig, dimB, dimA,tltrX,tltrY,trbrX,trbrY)
 
-            cv2.putText(orig, "{:.1f}in".format(dimA),
-                        (int(trbrX + 10), int(trbrY)), cv2.FONT_HERSHEY_SIMPLEX,
-                        0.65, (0, 0, 0), 2)
-            
         print(str(round(dimA,2)) + " " + str(round(dimB,2)))
-
 
         cv2.imshow("final", orig)
         cv2.waitKey(0)
@@ -147,6 +155,5 @@ sideCam = returnPlantSize(sideImage.jpg,0.955)
 #post to doc SideImage
 
 '''
-
 
 main()
