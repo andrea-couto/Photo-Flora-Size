cc9a2cecab58217546531d8ffa71e8c210778bc1
andrea-couto <acouto@student.bridgew.edu>
2018-05-19 17:32:19 -0400

    broke up returnPlantSize into more functions

--- returnPlantSize.py
+++ returnPlantSize.py
@@ -9,139 +9,137 @@ from imutils import contours
 import imutils
 from imutils import perspective
 
-
-def midpoint(ptA, ptB):
-    return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
-
 '''
 maybe we shoud'nt be taking the image as a parameter because we dont know what the image name will be
 how about we grab a list of all the images in the images folder, then while list is not empty,
 we take the image and process it, then delete it when done processing. '''
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True,
-                help="path to the input image")
-ap.add_argument("-w", "--width", type=float, required=True,
-                help="width of the left-most object in the image (in inches)")
-args = vars(ap.parse_args())
-
-
+#Global Constants
 lowerBound = np.array([33, 80, 40])
 upperBound = np.array([102, 255, 255])
-
-img = cv2.imread(args["image"],1)
-
 kernelOpen = np.ones((1, 1))
 kernelClose = np.ones((20, 20))
 
-img = cv2.resize(img, (340, 220))
+def midpoint(ptA, ptB):
+    return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
 
-# Getting green contour
+def makeCommandArguments():
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--image", required=True,
+                    help="path to the input image")
+    ap.add_argument("-w", "--width", type=float, required=True,
+                    help="width of the left-most object in the image (in inches)")
+    args = vars(ap.parse_args())
+    return args
 
-# convert BGR to HSV
-imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-# create the Mask
-mask = cv2.inRange(imgHSV, lowerBound, upperBound)
-# morphology
-maskOpen = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernelOpen)
-maskClose = cv2.morphologyEx(maskOpen, cv2.MORPH_CLOSE, kernelClose)
+def returnGreenObjects(img):
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(imgHSV, lowerBound, upperBound)
+    maskOpen = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernelOpen)
+    maskClose = cv2.morphologyEx(maskOpen, cv2.MORPH_CLOSE, kernelClose)
 
-maskFinal = maskClose
-_, conts, h = cv2.findContours(maskFinal.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    maskFinal = maskClose
+    _, conts, h = cv2.findContours(maskFinal.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    return conts
 
-# For getting reference Object
+def returnReferenceObject():
+    gray = cv2.GaussianBlur(img, (7, 7), 0)
+    edged = cv2.Canny(gray, 50, 100)
+    edged = cv2.dilate(edged, None, iterations=1)
+    edged = cv2.erode(edged, None, iterations=1)
+    Othercnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    ref = Othercnts[0] if imutils.is_cv2() else Othercnts[1]
+    ref = ref[0]
+    return ref
 
-gray = cv2.GaussianBlur(img, (7, 7), 0)
+def computeBoundingBox(c)
+        box = cv2.minAreaRect(c)
+        box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
+        box = np.array(box, dtype="int")
+        return box
 
-# perform edge detection, then perform a dilation + erosion to
-# close gaps in between object edges
-edged = cv2.Canny(gray, 50, 100)
-edged = cv2.dilate(edged, None, iterations=1)
-edged = cv2.erode(edged, None, iterations=1)
+def main():
+    args = makeCommandArguments()
+    img = cv2.imread(args["image"],1)
+    img = cv2.resize(img, (340, 220))
 
-# find contours in the edge map
-Othercnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-ref = Othercnts[0] if imutils.is_cv2() else Othercnts[1]
+    conts = returnGreenObjects(img)
+    ref = returnReferenceObject(img)
 
-ref = ref[0]
+    (cnts, _) = contours.sort_contours(conts)
+    pixelsPerMetric = None
 
-(cnts, _) = contours.sort_contours(conts)
-pixelsPerMetric = None
+    cnts = (ref,) + cnts
 
-# add reference object to list of plant objects
-cnts = (ref,)+cnts
+    for c in cnts:
+        # if the contour is not sufficiently large, ignore it
+        if cv2.contourArea(c) < 100:
+            continue
 
+        orig = img.copy()
 
-for c in cnts:
-    # if the contour is not sufficiently large, ignore it
-    if cv2.contourArea(c) < 100:
-        continue
+        box = computeBoundingBox(c)
 
-    # compute the rotated bounding box of the contour
-    orig = img.copy()
-    box = cv2.minAreaRect(c)
-    box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
-    box = np.array(box, dtype="int")
+        # order the points in the contour such that they appear
+        # in top-left, top-right, bottom-right, and bottom-left
+        # order, then draw the outline of the rotated bounding
+        # box
+        box = perspective.order_points(box)
+        cv2.drawContours(orig, [box.astype("int")], -1, (0, 255, 0), 2)
 
-    # order the points in the contour such that they appear
-    # in top-left, top-right, bottom-right, and bottom-left
-    # order, then draw the outline of the rotated bounding
-    # box
-    box = perspective.order_points(box)
-    cv2.drawContours(orig, [box.astype("int")], -1, (0, 255, 0), 2)
+        # loop over the original points and draw them
+        for (x, y) in box:
+            cv2.circle(orig, (int(x), int(y)), 5, (0, 0, 255), -1)
+            # unpack the ordered bounding box, then compute the midpoint
+            # between the top-left and top-right coordinates, followed by
+            # the midpoint between bottom-left and bottom-right coordinates
+            (tl, tr, br, bl) = box
+            (tltrX, tltrY) = midpoint(tl, tr)
+            (blbrX, blbrY) = midpoint(bl, br)
 
-    # loop over the original points and draw them
-    for (x, y) in box:
-        cv2.circle(orig, (int(x), int(y)), 5, (0, 0, 255), -1)
-        # unpack the ordered bounding box, then compute the midpoint
-        # between the top-left and top-right coordinates, followed by
-        # the midpoint between bottom-left and bottom-right coordinates
-        (tl, tr, br, bl) = box
-        (tltrX, tltrY) = midpoint(tl, tr)
-        (blbrX, blbrY) = midpoint(bl, br)
+            # compute the midpoint between the top-left and top-right points,
+            # followed by the midpoint between the top-righ and bottom-right
+            (tlblX, tlblY) = midpoint(tl, bl)
+            (trbrX, trbrY) = midpoint(tr, br)
 
-        # compute the midpoint between the top-left and top-right points,
-        # followed by the midpoint between the top-righ and bottom-right
-        (tlblX, tlblY) = midpoint(tl, bl)
-        (trbrX, trbrY) = midpoint(tr, br)
+            # draw the midpoints on the image
+            cv2.circle(orig, (int(tltrX), int(tltrY)), 5, (255, 0, 0), -1)
+            cv2.circle(orig, (int(blbrX), int(blbrY)), 5, (255, 0, 0), -1)
+            cv2.circle(orig, (int(tlblX), int(tlblY)), 5, (255, 0, 0), -1)
+            cv2.circle(orig, (int(trbrX), int(trbrY)), 5, (255, 0, 0), -1)
 
-        # draw the midpoints on the image
-        cv2.circle(orig, (int(tltrX), int(tltrY)), 5, (255, 0, 0), -1)
-        cv2.circle(orig, (int(blbrX), int(blbrY)), 5, (255, 0, 0), -1)
-        cv2.circle(orig, (int(tlblX), int(tlblY)), 5, (255, 0, 0), -1)
-        cv2.circle(orig, (int(trbrX), int(trbrY)), 5, (255, 0, 0), -1)
+            # draw lines between the midpoints
+            cv2.line(orig, (int(tltrX), int(tltrY)), (int(blbrX), int(blbrY)),
+                     (255, 0, 255), 2)
+            cv2.line(orig, (int(tlblX), int(tlblY)), (int(trbrX), int(trbrY)),
+                     (255, 0, 255), 2)
 
-        # draw lines between the midpoints
-        cv2.line(orig, (int(tltrX), int(tltrY)), (int(blbrX), int(blbrY)),
-                 (255, 0, 255), 2)
-        cv2.line(orig, (int(tlblX), int(tlblY)), (int(trbrX), int(trbrY)),
-                 (255, 0, 255), 2)
+            # compute the Euclidean distance between the midpoints
+            dA = dist.euclidean((tltrX, tltrY), (blbrX, blbrY))
+            dB = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
 
-        # compute the Euclidean distance between the midpoints
-        dA = dist.euclidean((tltrX, tltrY), (blbrX, blbrY))
-        dB = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
+            # if the pixels per metric has not been initialized, then
+            # compute it as the ratio of pixels to supplied metric
+            # (in this case, inches)
+            if pixelsPerMetric is None:
+                pixelsPerMetric = dB / args["width"]
 
-        # if the pixels per metric has not been initialized, then
-        # compute it as the ratio of pixels to supplied metric
-        # (in this case, inches)
-        if pixelsPerMetric is None:
-            pixelsPerMetric = dB / args["width"]
+                # compute the size of the object
+            dimA = dA / pixelsPerMetric
+            dimB = dB / pixelsPerMetric
+            print(dimA)
+            print(dimB)
 
-            # compute the size of the object
-        dimA = dA / pixelsPerMetric
-        dimB = dB / pixelsPerMetric
-        print(dimA)
-        print(dimB)
+            # draw the object sizes on the image
+            cv2.putText(orig, "{:.1f}in".format(dimB),
+                        (int(tltrX - 15), int(tltrY - 10)), cv2.FONT_HERSHEY_SIMPLEX,
+                        0.65, (0, 0, 0), 2)
 
-        # draw the object sizes on the image
-        cv2.putText(orig, "{:.1f}in".format(dimB),
-                    (int(tltrX - 15), int(tltrY - 10)), cv2.FONT_HERSHEY_SIMPLEX,
-                    0.65, (0, 0, 0), 2)
+            cv2.putText(orig, "{:.1f}in".format(dimA),
+                        (int(trbrX + 10), int(trbrY)), cv2.FONT_HERSHEY_SIMPLEX,
+                        0.65, (0, 0, 0), 2)
 
-        cv2.putText(orig, "{:.1f}in".format(dimA),
-                    (int(trbrX + 10), int(trbrY)), cv2.FONT_HERSHEY_SIMPLEX,
-                    0.65, (0, 0, 0), 2)
+        cv2.imshow("final", orig)
+        cv2.waitKey(0)
 
-    cv2.imshow("final", orig)
-    cv2.waitKey(0)
+main()
