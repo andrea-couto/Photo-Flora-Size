15dcfecf5c6ec25763ebe9b0dbe9fa7f4dfb092e
andrea-couto <acouto@student.bridgew.edu>
2018-05-30 22:30:31 -0400

    breaking onceaday into more functions

--- onceADay.py
+++ onceADay.py
@@ -9,11 +9,7 @@ import os
 import requests
 
 '''
-When reading and writing values from log, firebase, etc. 
-we are processing the measurements by top measurement first then 
-side measurement. 
-When retriecing and posting the data index 0 will be top and 1
-will be side
+Our convention is posting/pulling top measurement first, then side measurement.
 
 log will be:
 DATE HEIGHT WIDTH //for topmeasurement
@@ -22,12 +18,14 @@ DATE HEIGHT WIDTH //for sidemeasurement
 
 def checkConnection():
     try:
-        resp = requests.get("http://www.google.com")
+        resp = requests.get("http://www.firebase.com")
         return resp.status_code        
     except:
          return -1
 
+
 #add picNametop and picNameSide for adding dates to log
+#maybe eventually just posting area of bounding box
 def WriteToLog(height, width, logFileName):
     lf=open(logFileName,'a')
     lf.write(height + " " + width + "\n")
@@ -40,38 +38,52 @@ def ReadLog(logFileNm):
 	for line in lf:
 	#TODO break down line into elements
 		cachedData.append(line)
-	# fn=lf.read()
 	lf.close()
 	return cachedData
 
-# # Want this running on your pi? 
-# # Youll need your own firebase database and serviceAccountKey
-cred = credentials.Certificate("./serviceAccountKey.json")
-defaultApp = firebase_admin.initialize_app(cred)
-db = firestore.client()
+
+def connectDB(serviceAccountFileName):
+	try:
+		cred = credentials.Certificate(serviceAccountFileName)
+		defaultApp = firebase_admin.initialize_app(cred)
+		db = firestore.client()
+		return db
+	except:
+		print("Something went wrong initializing the firebase app")
+
+
+def postData(db, collectionName, documentName, height, width):
+	docRef = db.collection(collectionName).document(documentName)
+	docRef.set({
+	    u'measurement 1': height,
+	    u'measurement 2': width,
+	  })
+
 
 # '''
 # We should not fail miserably when the program doesnt find a reference/plant object in the photo
 # '''
 
-picNameTop, picNameSide = takePhotos.takePhotos()
+def main():
+	picNameTop, picNameSide = takePhotos.takePhotos()
 
-top = returnPlantSize.calculateAndDisplay(picNameTop, 0.955)
-side = returnPlantSize.calculateAndDisplay(picNameSide, 0.955)
+	topData = returnPlantSize.calculateAndDisplay(picNameTop, 0.955)
+	sideData = returnPlantSize.calculateAndDisplay(picNameSide, 0.955)
 
-#put in function and test to make sure they are 1"x1"
-topRef = top[0]
-sideRef = side[0]
+	#put in function and test to make sure they are 1"x1"
+	topRef = topData[0]
+	sideRef = sideData[0]
 
-topMeasurement = top[1]
-sideMeasurement = side[1]
+	topMeasurement = topData[1]
+	sideMeasurement = sideData[1]
 
-os.remove(helperFunctions.setDirPathByOs() + picNameSide)
-os.remove(helperFunctions.setDirPathByOs() + picNameTop)
+	os.remove(helperFunctions.setDirPathByOs() + picNameSide)
+	os.remove(helperFunctions.setDirPathByOs() + picNameTop)
 
 
-if (checkConnection() == 200):
-	cachedData = ReadLog("TEMPLOG.txt")
+	if (checkConnection() == 200):
+		db = connectDB("./serviceAccountKey.json")
+		cachedData = ReadLog("TEMPLOG.txt")
 # 	#if cachedData.size() !=0:
 		#for element in cachedData:
 			#for part in element:
@@ -79,22 +91,16 @@ if (checkConnection() == 200):
 				#KEEP IN MIND TOP AND SIDE MEASUREMENTS FOR EACH DATE
 				#post parts to firebase
 
-	docRefSide = db.collection("sideMeasurements").document(picNameSide)
-	docRefSide.set({
-	    u'measurement 1': sideMeasurement[0],
-	    u'measurement 2': sideMeasurement[1],
+		postData(db, "sideMeasurements", picNameSide, sideMeasurement[0], sideMeasurement[1])
+		postData(db, "topMeasurements", picNameTop, topMeasurement[0], topMeasurement[1])
 
-	  })
-	docRefTop = db.collection("topMeasurements").document(picNameTop)
-	docRefTop.set({
-	    u'measurement 1': topMeasurement[0],
-	    u'measurement 2': topMeasurement[1],
+		print( str(sideMeasurement) + " measurement was uploaded")
+		print( str(topMeasurement) + " measurement was uploaded")
+	else:
+		WriteToLog(topMeasurement[0], topMeasurement[1], "TEMPLOG.txt")
+		WriteToLog(topMeasurement[0], topMeasurement[1], "TEMPLOG.txt")
+		#Add a clear log function?
 
-	  })
-	print( str(sideMeasurement) + " measurement was uploaded")
-	print( str(topMeasurement) + " measurement was uploaded")
-else:
-	WriteToLog(topMeasurement[0], topMeasurement[1], "TEMPLOG.txt")
-	WriteToLog(topMeasurement[0], topMeasurement[1], "TEMPLOG.txt")
-	#Add a clear log function?
 
+if __name__ == '__main__':
+	main()
